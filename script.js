2a620dca492346cdb03aa7d5307e97d2c22b4584
Hridoy2018 <hridoyhridoy19@yahoo.com>
2018-05-21 04:47:04 +0000

    class_work_1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* JavaScript code here */
   console.log("Internal Js")
+  //Task 2
+  
   
   function sumArray(arr){
       var sum =0;
@@ -10,3 +12,19 @@
       return sum;
   }
   console.log(sumArray([1,2,3,4,5]));
+ console.log(sumArray([1,20,-3,45]));
+
+
+//Task 3
+ 
+ 
+ function checkEmail(emailString)
+ {var emailFormat=/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+ var match =emailFormat.test(emailString);
+ return match;
+ }
+ console.log(checkEmail("John@smith.com"));
+ console.log(checkEmail("Johnsmith.com"));
+ 
+ //Task 4
+ 
