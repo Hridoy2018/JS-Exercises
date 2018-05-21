1704548de71813e29749d2e7ea714b18025e6f4e
Hridoy2018 <hridoyhridoy19@yahoo.com>
2018-05-21 04:55:47 +0000

    class_work_4

--- script.js
+++ script.js
@@ -28,3 +28,28 @@
  
  //Task 4
  
+ var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+
+ 
+ function getReadingStatua(index){
+     return library[index].readingStatus;
+     
+ }
+ console.log(getReadingStatua(1));
+ 
