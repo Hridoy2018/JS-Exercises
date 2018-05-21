0be27a7c1e163e65b2fc8412dccae3446d6c05e9
Hridoy2018 <hridoyhridoy19@yahoo.com>
2018-05-21 04:33:37 +0000

    class_work_3

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 /* JavaScript code here */
   console.log("Internal Js")
+  
+  function sumArray(arr){
+      var sum =0;
+      arr.forEach(function(value,index)
+      {sum += value;
+          
+      });
+      return sum;
+  }
+  console.log(sumArray([1,2,3,4,5]));
