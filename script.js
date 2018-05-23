314f4d6e3419b6aa812557f3bc664af887ebc882
Hridoy2018 <hridoyhridoy19@yahoo.com>
2018-05-23 04:16:00 +0000

    Task_5_a

--- script.js
+++ script.js
@@ -53,3 +53,30 @@
  }
  console.log(getReadingStatua(1));
  
+ //Task 5
+ 
+ var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+
+
+function addItem(newItem){
+    cart.push(newItem);
+    console.log(cart);
+    
+}
+var newItem = {name: 'watch' , price: 64, quantity: 1};
+addItem(newItem);
